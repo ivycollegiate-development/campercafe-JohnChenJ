47deb5cb8cba6528b8492da28adc7397b371a3ce
JohnChenJ <jchen23_student@ivycollegiate.org>
2022-11-25 15:47:02 +0800

    Added variables.py with some variables.

---
+++ .github/assets/variables.py
@@ -0,0 +1,17 @@
+#Variable: The container for information
+a = 28
+b = 1.5
+c = "Hello!"
+d = True
+e = None
+
+'''
+a: an integer
+b: a decimal number(floating point)
+c: string or character(s)
+d: bool(True or false)
+e: NoneType
+'''
+
+name = input("Put Your Name Here: ")
+print ("Hello, " + name)
