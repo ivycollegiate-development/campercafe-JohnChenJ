bc84ab82a9025c9bc9010f3160326aca82408e50
JohnChenJ <jchen23_student@ivycollegiate.org>
2022-11-25 16:13:02 +0800

    Added conditions.py

---
+++ .github/assets/conditions.py
@@ -0,0 +1,9 @@
+num = input("Number: ")
+if num > 0:
+    print("Number is positive")
+elif num < 0:
+    print("Number is negative")
+else:
+    print("Number is zero")
+#In Python, tab is very crucial, just like Java
+#Anyway, put tab to organize your code
