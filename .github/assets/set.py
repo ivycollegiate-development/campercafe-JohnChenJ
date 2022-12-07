12ae6f322cfdd6d36fd87a63cf0e90417f93d22c
JohnChenJ <jchen23_student@ivycollegiate.org>
2022-12-07 16:03:07 +0800

    set.py added and code setup

---
+++ .github/assets/set.py
@@ -0,0 +1,20 @@
+#This is how you create an empty set
+s = set()
+
+'''
+set is more than just a string
+not a database yet
+set is like a tiny database
+Tons of set make up a database
+'''
+#and this is how you add elements to the set:
+s.add(1)
+'''
+set TAKE EXACTLY ONE ARGUMENT
+'''
+#s.add(2,3)
+s.add(2)
+
+#This is how you print and view the contents of the set that you've add into.
+
+print(s)
