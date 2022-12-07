7a63b3a8c0ba7d54f33b2beedb4c20d738079a11
JohnChenJ <jchen23_student@ivycollegiate.org>
2022-12-07 15:44:33 +0800

    Note for string name

--- .github/assets/hello.py
+++ .github/assets/hello.py
@@ -1,1 +1,11 @@
 print("Hello, world!")
+name = "Harry"
+"""
+This is how you comment in Python
+Multicomment
+"""
+#Single comment
+print(name[0]) #Computer start from 0, but exclude 6 if (0,6)
+print(name[1])
+print(name[2])
+print(name[3])
