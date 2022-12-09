039bc42b114ec1ac4f6e5853bd370bbfe9559fb2
JohnChenJ <jchen23_student@ivycollegiate.org>
2022-12-09 16:13:23 +0800

    dict.py note upgraded

--- .github/assets/dict.py
+++ .github/assets/dict.py
@@ -1,5 +1,13 @@
 #This is how to define a dictionary file in python
 houses = {"Harry" : "Gryffindor", "Draco":"Slytherin"}
 
+#Dictionary file:"Harry", and we put "Gryffindor" into it.
+
 #How to print dict. Look at those purple blanket
 print(houses["Harry"])
+
+#Different component in different place reason: Don't need to worry when one of the machine burned
+#Dict is a new type that Python have
+
+#The actual program is *infintely more complex than this
+# There is all open sources
