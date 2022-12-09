0c8add3ca8e7a78dbec20d3bd2d863dd0b31155a
JohnChenJ <jchen23_student@ivycollegiate.org>
2022-12-09 16:04:55 +0800

    added dict.py

---
+++ .github/assets/dict.py
@@ -0,0 +1,5 @@
+#This is how to define a dictionary file in python
+houses = {"Harry" : "Gryffindor", "Draco":"Slytherin"}
+
+#How to print dict. Look at those purple blanket
+print(houses["Harry"])
